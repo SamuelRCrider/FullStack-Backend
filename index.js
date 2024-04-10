af3396665ae17a486c79a18db46f3172fc573239
SamuelRCrider <sam.crider@aol.com>
2024-04-10 12:58:10 -0400

    changed PORT to work on Render deployment

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.delete("/api/persons/:id", (req, res) => {
 
 // app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
